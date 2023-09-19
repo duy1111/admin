c33cd5758f7a90fd8c9ee0e6ad5fb90b4cc65eda
duy1111 <maixuanduy0605@gmail.com>
2023-09-19 15:57:27 +0700

    clone base code

--- src/App.tsx
+++ src/App.tsx
@@ -14,31 +14,17 @@ import routerProvider, {
 } from "@refinedev/react-router-v6";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import {
-    ShoppingOutlined,
     UsergroupAddOutlined,
-    ShopOutlined,
-    StarOutlined,
     DashboardOutlined,
 } from "@ant-design/icons";
 import jsonServerDataProvider from "@refinedev/simple-rest";
 import { authProvider } from "./authProvider";
-
 import "dayjs/locale/de";
-
 import { DashboardPage } from "./pages/dashboard";
-import { OrderList, OrderShow } from "./pages/orders";
 import { AuthPage } from "./pages/auth";
 import { UserList, UserShow } from "./pages/users";
-import {
-    CourierList,
-    CourierShow,
-    CourierCreate,
-    CourierEdit,
-} from "./pages/couriers";
 import { ProductList } from "./pages/products";
-import { StoreCreate, StoreEdit, StoreList } from "./pages/stores";
 import { CategoryList } from "./pages/categories";
-import { ReviewsList } from "./pages/reviews";
 import { useTranslation } from "react-i18next";
 import { Header, Title, OffLayoutArea } from "./components";
 import { BikeWhiteIcon, PizzaIcon } from "./components/icons";
@@ -83,14 +69,6 @@ const App: React.FC = () => {
                                 },
                             },
                             {
-                                name: "orders",
-                                list: "/orders",
-                                show: "/orders/show/:id",
-                                meta: {
-                                    icon: <ShoppingOutlined />,
-                                },
-                            },
-                            {
                                 name: "users",
                                 list: "/users",
                                 show: "/users/show/:id",
@@ -106,35 +84,10 @@ const App: React.FC = () => {
                                 },
                             },
                             {
-                                name: "stores",
-                                list: "/stores",
-                                create: "/stores/create",
-                                edit: "/stores/edit/:id",
-                                meta: {
-                                    icon: <ShopOutlined />,
-                                },
-                            },
-                            {
                                 name: "categories",
                                 list: "/categories",
                             },
-                            {
-                                name: "couriers",
-                                list: "/couriers",
-                                create: "/couriers/create",
-                                edit: "/couriers/edit/:id",
-                                show: "/couriers/show/:id",
-                                meta: {
-                                    icon: <BikeWhiteIcon />,
-                                },
-                            },
-                            {
-                                name: "reviews",
-                                list: "/reviews",
-                                meta: {
-                                    icon: <StarOutlined />,
-                                },
-                            },
+
                         ]}
                     >
                         <Routes>
@@ -157,14 +110,6 @@ const App: React.FC = () => {
                             >
                                 <Route index element={<DashboardPage />} />
 
-                                <Route path="/orders">
-                                    <Route index element={<OrderList />} />
-                                    <Route
-                                        path="show/:id"
-                                        element={<OrderShow />}
-                                    />
-                                </Route>
-
                                 <Route path="/users">
                                     <Route index element={<UserList />} />
                                     <Route
@@ -177,44 +122,12 @@ const App: React.FC = () => {
                                     path="/products"
                                     element={<ProductList />}
                                 />
-
-                                <Route path="/stores">
-                                    <Route index element={<StoreList />} />
-                                    <Route
-                                        path="create"
-                                        element={<StoreCreate />}
-                                    />
-                                    <Route
-                                        path="edit/:id"
-                                        element={<StoreEdit />}
-                                    />
-                                </Route>
-
                                 <Route
                                     path="/categories"
                                     element={<CategoryList />}
                                 />
 
-                                <Route path="/couriers">
-                                    <Route index element={<CourierList />} />
-                                    <Route
-                                        path="create"
-                                        element={<CourierCreate />}
-                                    />
-                                    <Route
-                                        path="edit/:id"
-                                        element={<CourierEdit />}
-                                    />
-                                    <Route
-                                        path="show/:id"
-                                        element={<CourierShow />}
-                                    />
-                                </Route>
 
-                                <Route
-                                    path="/reviews"
-                                    element={<ReviewsList />}
-                                />
                             </Route>
 
                             <Route
